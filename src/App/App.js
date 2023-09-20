c1a0cf616349074a0444ca09b200d9ff48be0573
Scotty Brown <scottybrown98@gmail.com>
2023-09-20 17:16:41 -0500

    Add header component

---
+++ src/App/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Header from '../Header/Header';
+import { NavLink } from 'react-router-dom';
+
+function App() {
+
+  
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
