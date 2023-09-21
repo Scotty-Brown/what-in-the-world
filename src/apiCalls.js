86c820e52ff898fda1593d294149ae677523243d
Scotty Brown <scottybrown98@gmail.com>
2023-09-21 18:03:47 -0500

    Add fetch Get for country details

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -7,3 +7,14 @@ export function fetchSimple() {
         return res.json()
     })
 }
+
+export function fetchCountryDetails(countryCode) {
+    return fetch(`https://restcountries.com/v3.1/alpha/${countryCode}`)
+            .then(res => {
+                if(!res.ok) {
+                    throw new Error(`${res.status}`)
+                }
+                
+                return res.json()
+            })
+}
