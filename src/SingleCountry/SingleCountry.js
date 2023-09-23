4e2e25bc7f0c42fbd69216ef84d48299bd53b85b
Scotty Brown <scottybrown98@gmail.com>
2023-09-23 01:43:08 -0500

    Updated conditional rendering

--- src/SingleCountry/SingleCountry.js
+++ src/SingleCountry/SingleCountry.js
@@ -38,51 +38,62 @@ function SingleCountry() {
     }
 
     function getNativeLanguageCode(data) {
-        const languageCode = Object.keys(data.name.nativeName)[0]
-        setNativeLanguageCode(languageCode)
+        if(data.name.nativeName) {
+            const languageCode = Object.keys(data.name.nativeName)[0]
+            setNativeLanguageCode(languageCode)
+        } 
     }
 
     function getCurrencyCode(data) {
-        const code = Object.keys(data.currencies)[0]
-        setCurrencyCode(code)
+        if(data.currencies) {
+            const code = Object.keys(data.currencies)[0]
+            setCurrencyCode(code)
+        } 
     }
+
+    
 
     return (
         <div className='country-details-container'>
-            {countryData && borderCountries && nativeLanguageCode && currencyCode && (
-                <>
-                    <div className='graphics-container'>
-                        <p className='graphic-title'>{countryData.name?.common} Flag</p>
-                        <img alt={countryData.flags?.alt} src={countryData.flags?.png}></img>
-                        {countryData.coatOfArms?.png ? 
+        {countryData && borderCountries && (
+            <>
+                <div className='graphics-container'>
+                    <p className='graphic-title'>{countryData.name?.common} Flag</p>
+                    <img alt={countryData.flags?.alt} src={countryData.flags?.png}></img>
+                    {countryData.coatOfArms?.png && (
                         <>
-                        <p className='graphic-title'>{countryData.name?.common} Coat of Arms</p>
-                        <img alt={countryData.coatOfArms?.alt} src={countryData.coatOfArms?.png}></img>
-                        </> : null}
+                            <p className='graphic-title'>{countryData.name?.common} Coat of Arms</p>
+                            <img alt={countryData.coatOfArms?.alt} src={countryData.coatOfArms?.png}></img>
+                        </>
+                    )}
+                </div>
+                <div className='data-style-div'>
+                    <div className='main-data-container'>
+                        <p className='data-details'>Common Name: {countryData.name?.common || 'No Data Available'}</p>
+                        <p className='data-details'>Native Name: {countryData.name?.nativeName?.[nativeLanguageCode]?.official || 'No Data Available'}</p>
+                        <p className='data-details'>Capital: {countryData.capital || 'No Data Available'}</p>
+                        <p className='data-details'>Common Language Spoken: {countryData.languages?.[nativeLanguageCode] || 'No Data Available'}</p>
+                        <p className='data-details'>Population: {countryData.population || 'No Data Available'}</p>
+                        <p className='data-details'>Currencies: {countryData.currencies?.[currencyCode]?.name || 'No Data Available'}</p>
+                        <p className='data-details'>Cars drive on the {countryData.car?.side || 'No Data Available'} side of the road.</p>
                     </div>
-                    <div className='data-style-div'>
-                        <div className='main-data-container'>
-                            <p className='data-details'>Common Name: {countryData.name?.common}</p>
-                            <p className='data-details'>Native Name: {countryData.name?.nativeName[nativeLanguageCode]?.official}</p>
-                            <p className='data-details'>Capital: {countryData.capital}</p>
-                            <p className='data-details'>Common Language Spoken: {countryData.languages[nativeLanguageCode]}</p>
-                            <p className='data-details'>Population: {countryData.population}</p>
-                            <p className='data-details'>Currencies: {countryData.currencies[currencyCode]?.name}</p>
-                            <p className='data-details'>Cars drive on the {countryData.car?.side} side of the road.</p>
-                        </div>
-                        <div className='graphical-data-container'>
-                            <p className='data-details'>Region: {countryData.region}</p>
-                            <p className='data-details'>Subregion: {countryData.subregion}</p>
-                            <p className='data-details'>Continent: {countryData.continents}</p>
-                            {borderCountries.length > 1 ? <p className='data-details'>Border Countries: {borderCountries} </p> : <p className='data-details'>No Bordering Countries</p>}
-                            <Link className='google-map-link' to={countryData.maps?.googleMaps} target='_blank' rel='noopener noreferrer'>
-                                View Location in Google Maps
-                            </Link>
-                        </div>
+                    <div className='graphical-data-container'>
+                        <p className='data-details'>Region: {countryData.region || 'No Data Available'}</p>
+                        <p className='data-details'>Subregion: {countryData.subregion || 'No Data Available'}</p>
+                        <p className='data-details'>Continent: {countryData.continents || 'No Data Available'}</p>
+                        {borderCountries.length > 1 ? (
+                            <p className='data-details'>Border Countries: {borderCountries} </p>
+                        ) : (
+                            <p className='data-details'>No Bordering Countries</p>
+                        )}
+                        <Link className='google-map-link' to={countryData.maps?.googleMaps} target='_blank' rel='noopener noreferrer'>
+                            View Location in Google Maps
+                        </Link>
                     </div>
-                </>
-            )}
-        </div>
+                </div>
+            </>
+        )}
+    </div>
     )
 }
 
