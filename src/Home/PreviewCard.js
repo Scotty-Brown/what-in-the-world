399fb69746983f2e104748919601b78b4897289e
Scotty Brown <scottybrown98@gmail.com>
2023-09-22 19:59:55 -0500

    Update styling

--- src/Home/PreviewCard.js
+++ src/Home/PreviewCard.js
@@ -3,7 +3,7 @@ import './PreviewCard.css'
 
 function PreviewCard({name, flag, id, alt}) {
     return (
-        <Link to={`/${id}`} >
+        <Link to={`/${id}`} className='preview-card-link'>
             <div id={id} className="preview-card">
                 <img alt={alt}  src={flag}></img>
                 <p className='preview-card-name'>{name}</p>
--- src/SingleCountry/SingleCountry.js
+++ src/SingleCountry/SingleCountry.js
@@ -52,26 +52,33 @@ function SingleCountry() {
             {countryData && borderCountries && nativeLanguageCode && currencyCode && (
                 <>
                     <div className='graphics-container'>
+                        <p className='graphic-title'>{countryData.name?.common} Flag</p>
                         <img alt={countryData.flags?.alt} src={countryData.flags?.png}></img>
+                        {countryData.coatOfArms?.png ? 
+                        <>
+                        <p className='graphic-title'>{countryData.name?.common} Coat of Arms</p>
                         <img alt={countryData.coatOfArms?.alt} src={countryData.coatOfArms?.png}></img>
+                        </> : null}
                     </div>
-                    <div className='main-data-container'>
-                        <p>Common Name: {countryData.name?.common}</p>
-                        <p>Native Name: {countryData.name?.nativeName[nativeLanguageCode]?.official}</p>
-                        <p>Capital: {countryData.capital}</p>
-                        <p>Common Language Spoken: {countryData.languages[nativeLanguageCode]}</p>
-                        <p>Population: {countryData.population}</p>
-                        <p>Currencies: {countryData.currencies[currencyCode]?.name}</p>
-                        <p>Cars drive on the {countryData.car?.side} side of the road.</p>
-                    </div>
-                    <div className='graphical-data-container'>
-                        <p>Region: {countryData.region}</p>
-                        <p>Subregion: {countryData.subregion}</p>
-                        <p>Continent: {countryData.continents}</p>
-                        <p>Border Countries: {borderCountries} </p>
-                        <Link className='google-map' to={countryData.maps?.googleMaps} target='_blank' rel='noopener noreferrer'>
-                            View Location in Google Maps
-                        </Link>
+                    <div className='data-style-div'>
+                        <div className='main-data-container'>
+                            <p className='data-details'>Common Name: {countryData.name?.common}</p>
+                            <p className='data-details'>Native Name: {countryData.name?.nativeName[nativeLanguageCode]?.official}</p>
+                            <p className='data-details'>Capital: {countryData.capital}</p>
+                            <p className='data-details'>Common Language Spoken: {countryData.languages[nativeLanguageCode]}</p>
+                            <p className='data-details'>Population: {countryData.population}</p>
+                            <p className='data-details'>Currencies: {countryData.currencies[currencyCode]?.name}</p>
+                            <p className='data-details'>Cars drive on the {countryData.car?.side} side of the road.</p>
+                        </div>
+                        <div className='graphical-data-container'>
+                            <p className='data-details'>Region: {countryData.region}</p>
+                            <p className='data-details'>Subregion: {countryData.subregion}</p>
+                            <p className='data-details'>Continent: {countryData.continents}</p>
+                            {borderCountries.length > 1 ? <p className='data-details'>Border Countries: {borderCountries} </p> : <p className='data-details'>No Bordering Countries</p>}
+                            <Link className='google-map-link' to={countryData.maps?.googleMaps} target='_blank' rel='noopener noreferrer'>
+                                View Location in Google Maps
+                            </Link>
+                        </div>
                     </div>
                 </>
             )}
